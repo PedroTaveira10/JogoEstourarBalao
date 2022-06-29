bc16daacfcd61be869dfd8548d13b53c5dc61bb1
PedroTaveira10 <pedro.craque@hotmail.com>
2022-06-28 21:56:54 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+// Criar o balao
+// 2 estourar o balão
+// 3 carregar o jogo
+var total = 0;
+
+ function criarBalao(){
+  var balao = document.createElement("div");
+  balao.setAttribute("class","balao");
+
+  var x = Math.floor(Math.random() *600);
+  var y = Math.floor(Math.random() *400);
+
+  balao.setAttribute("style", "left:"+x+"px;top:"+y+"px;");
+  //<div class="balao" style="left:30px; top:60px"></div>
+  balao.setAttribute("onclick", "estourar(this)");
+
+  document.body.appendChild(balao);
+ }
+
+ function estourar(objeto){
+  document.body.removeChild(objeto);
+  total ++;
+  var score = document.getElementById("total");
+  score.innerHTML ="Balao estourados: " + total;
+
+ }
+
+ function carregarJogo(){
+  setInterval(criarBalao, 1000);
+
+ }
